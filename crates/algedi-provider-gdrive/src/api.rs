1cef3f29839ba780f542ae101ab2a19f6fe3bb42
Rodrigo Brito <rodrigo@w3ti.com.br>
2026-08-18 16:36:23 -0300

    Implement cloud file transfers

--- crates/algedi-provider-gdrive/src/api.rs
+++ crates/algedi-provider-gdrive/src/api.rs
@@ -1,11 +1,15 @@
 //! Thin wrapper over the Drive API v3 REST endpoints actually needed by the
 //! sync engine (metadata, upload, download, delete).
 
+use algedi_provider_trait::{ProviderError, ProviderResult};
 use serde::Deserialize;
+use std::path::{Path, PathBuf};
+use tokio::io::AsyncWriteExt;
+use tokio_util::io::ReaderStream;
 
 const API_BASE: &str = "https://www.googleapis.com/drive/v3";
-#[allow(dead_code)]
 const UPLOAD_BASE: &str = "https://www.googleapis.com/upload/drive/v3";
+const FIELDS: &str = "id,name,mimeType,md5Checksum,size,modifiedTime,parents";
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -34,7 +38,10 @@ pub struct GDriveApi {
 
 impl GDriveApi {
     pub fn new(access_token: String) -> Self {
-        Self { http: reqwest::Client::new(), access_token: std::sync::RwLock::new(access_token) }
+        Self {
+            http: reqwest::Client::new(),
+            access_token: std::sync::RwLock::new(access_token),
+        }
     }
 
     pub fn set_access_token(&self, token: String) {
@@ -45,40 +52,107 @@ impl GDriveApi {
         self.access_token.read().unwrap().clone()
     }
 
-    pub async fn get_file(&self, file_id: &str) -> algedi_provider_trait::ProviderResult<DriveFile> {
+    pub async fn get_file(
+        &self,
+        file_id: &str,
+    ) -> algedi_provider_trait::ProviderResult<DriveFile> {
         let url = format!(
             "{API_BASE}/files/{file_id}?fields=id,name,mimeType,md5Checksum,size,modifiedTime,parents"
         );
         self.get_json(&url).await
     }
 
-    pub async fn create_folder(
-        &self,
-        name: &str,
-        parent_id: &str,
-    ) -> algedi_provider_trait::ProviderResult<DriveFile> {
-        // TODO: POST {API_BASE}/files with mimeType=application/vnd.google-apps.folder
-        let _ = (name, parent_id);
-        Err(algedi_provider_trait::ProviderError::Other("not implemented".into()))
+    pub async fn create_folder(&self, name: &str, parent_id: &str) -> ProviderResult<DriveFile> {
+        let response = self
+            .http
+            .post(format!("{API_BASE}/files"))
+            .bearer_auth(self.access_token())
+            .query(&[("fields", FIELDS)])
+            .json(&serde_json::json!({
+                "name": name,
+                "mimeType": "application/vnd.google-apps.folder",
+                "parents": [parent_id]
+            }))
+            .send()
+            .await
+            .map_err(network_error)?;
+        decode_json(response, name).await
     }
 
     pub async fn upload_file(
         &self,
-        _local_path: &std::path::Path,
-        _parent_id: &str,
-    ) -> algedi_provider_trait::ProviderResult<DriveFile> {
-        // TODO: resumable upload session against UPLOAD_BASE for large
-        // files, simple multipart upload otherwise.
-        Err(algedi_provider_trait::ProviderError::Other("not implemented".into()))
+        local_path: &Path,
+        parent_id: &str,
+    ) -> ProviderResult<DriveFile> {
+        let name = local_path
+            .file_name()
+            .and_then(|n| n.to_str())
+            .ok_or_else(|| {
+                ProviderError::Other("local path has no valid UTF-8 file name".into())
+            })?;
+        let size = tokio::fs::metadata(local_path)
+            .await
+            .map_err(io_error)?
+            .len();
+        let response = self
+            .http
+            .post(format!("{UPLOAD_BASE}/files"))
+            .bearer_auth(self.access_token())
+            .query(&[("uploadType", "resumable"), ("fields", FIELDS)])
+            .header("X-Upload-Content-Type", "application/octet-stream")
+            .header("X-Upload-Content-Length", size)
+            .json(&serde_json::json!({"name": name, "parents": [parent_id]}))
+            .send()
+            .await
+            .map_err(network_error)?;
+        if !response.status().is_success() {
+            return Err(response_error(response, name).await);
+        }
+        let session = response
+            .headers()
+            .get(reqwest::header::LOCATION)
+            .and_then(|v| v.to_str().ok())
+            .map(str::to_owned)
+            .ok_or_else(|| {
+                ProviderError::Other("Drive resumable upload returned no Location header".into())
+            })?;
+        let file = tokio::fs::File::open(local_path).await.map_err(io_error)?;
+        let response = self
+            .http
+            .put(session)
+            .header(reqwest::header::CONTENT_LENGTH, size)
+            .header(reqwest::header::CONTENT_TYPE, "application/octet-stream")
+            .body(reqwest::Body::wrap_stream(ReaderStream::new(file)))
+            .send()
+            .await
+            .map_err(network_error)?;
+        decode_json(response, name).await
     }
 
-    pub async fn download_file(
-        &self,
-        _file_id: &str,
-        _dest_path: &std::path::Path,
-    ) -> algedi_provider_trait::ProviderResult<()> {
-        // TODO: GET /files/{id}?alt=media, streamed to disk.
-        Err(algedi_provider_trait::ProviderError::Other("not implemented".into()))
+    pub async fn download_file(&self, file_id: &str, dest_path: &Path) -> ProviderResult<()> {
+        let mut response = self
+            .http
+            .get(format!("{API_BASE}/files/{file_id}"))
+            .bearer_auth(self.access_token())
+            .query(&[("alt", "media")])
+            .send()
+            .await
+            .map_err(network_error)?;
+        if !response.status().is_success() {
+            return Err(response_error(response, file_id).await);
+        }
+        let temporary = temporary_path(dest_path);
+        let mut output = tokio::fs::File::create(&temporary)
+            .await
+            .map_err(io_error)?;
+        while let Some(chunk) = response.chunk().await.map_err(network_error)? {
+            output.write_all(&chunk).await.map_err(io_error)?;
+        }
+        output.flush().await.map_err(io_error)?;
+        drop(output);
+        tokio::fs::rename(&temporary, dest_path)
+            .await
+            .map_err(io_error)
     }
 
     pub async fn delete_file(&self, file_id: &str) -> algedi_provider_trait::ProviderResult<()> {
@@ -93,7 +167,9 @@ impl GDriveApi {
         if resp.status().is_success() {
             Ok(())
         } else {
-            Err(algedi_provider_trait::ProviderError::Other(resp.status().to_string()))
+            Err(algedi_provider_trait::ProviderError::Other(
+                resp.status().to_string(),
+            ))
         }
     }
 
@@ -115,10 +191,58 @@ impl GDriveApi {
                 .await
                 .map_err(|e| algedi_provider_trait::ProviderError::Other(e.to_string())),
             401 => Err(algedi_provider_trait::ProviderError::AuthRequired),
-            404 => Err(algedi_provider_trait::ProviderError::NotFound(url.to_string())),
-            429 => Err(algedi_provider_trait::ProviderError::RateLimited { retry_after_secs: None }),
-            other => Err(algedi_provider_trait::ProviderError::Other(format!("HTTP {other}"))),
+            404 => Err(algedi_provider_trait::ProviderError::NotFound(
+                url.to_string(),
+            )),
+            429 => Err(algedi_provider_trait::ProviderError::RateLimited {
+                retry_after_secs: None,
+            }),
+            other => Err(algedi_provider_trait::ProviderError::Other(format!(
+                "HTTP {other}"
+            ))),
         }
+    }
+}
+
+fn temporary_path(dest: &Path) -> PathBuf {
+    let mut name = dest.as_os_str().to_owned();
+    name.push(".algedi-part");
+    PathBuf::from(name)
+}
+
+fn network_error(error: reqwest::Error) -> ProviderError {
+    ProviderError::Network(error.to_string())
+}
+fn io_error(error: std::io::Error) -> ProviderError {
+    ProviderError::Other(error.to_string())
+}
+
+async fn decode_json<T: for<'de> Deserialize<'de>>(
+    response: reqwest::Response,
+    resource: &str,
+) -> ProviderResult<T> {
+    if response.status().is_success() {
+        response
+            .json()
+            .await
+            .map_err(|e| ProviderError::Other(e.to_string()))
+    } else {
+        Err(response_error(response, resource).await)
+    }
+}
+
+async fn response_error(response: reqwest::Response, resource: &str) -> ProviderError {
+    let status = response.status();
+    let retry_after_secs = response
+        .headers()
+        .get(reqwest::header::RETRY_AFTER)
+        .and_then(|v| v.to_str().ok())
+        .and_then(|v| v.parse().ok());
+    match status.as_u16() {
+        401 => ProviderError::AuthRequired,
+        404 => ProviderError::NotFound(resource.to_owned()),
+        429 => ProviderError::RateLimited { retry_after_secs },
+        code => ProviderError::Other(format!("HTTP {code}")),
     }
 }
 
@@ -132,5 +256,13 @@ mod tests {
         assert_eq!(api.access_token(), "old-token");
         api.set_access_token("new-token".into());
         assert_eq!(api.access_token(), "new-token");
+    }
+
+    #[test]
+    fn partial_download_uses_a_sibling_temporary_file() {
+        assert_eq!(
+            temporary_path(Path::new("/tmp/report.pdf")),
+            PathBuf::from("/tmp/report.pdf.algedi-part")
+        );
     }
 }
--- crates/algedi-provider-onedrive/src/api.rs
+++ crates/algedi-provider-onedrive/src/api.rs
@@ -1,9 +1,21 @@
 //! Thin wrapper over the Microsoft Graph `/me/drive` endpoints actually
 //! needed by the sync engine (metadata, upload, download, delete).
 
+use algedi_provider_trait::{ProviderError, ProviderResult};
+use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
 use serde::Deserialize;
+use std::path::{Path, PathBuf};
+use tokio::io::AsyncWriteExt;
 
 const GRAPH_BASE: &str = "https://graph.microsoft.com/v1.0";
+const SIMPLE_UPLOAD_LIMIT: u64 = 4 * 1024 * 1024;
+const UPLOAD_CHUNK_SIZE: usize = 10 * 320 * 1024;
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct UploadSession {
+    upload_url: String,
+}
 
 #[derive(Debug, Deserialize)]
 pub struct DriveItem {
@@ -48,7 +60,10 @@ pub struct GraphApi {
 
 impl GraphApi {
     pub fn new(access_token: String) -> Self {
-        Self { http: reqwest::Client::new(), access_token: std::sync::RwLock::new(access_token) }
+        Self {
+            http: reqwest::Client::new(),
+            access_token: std::sync::RwLock::new(access_token),
+        }
     }
 
     pub fn set_access_token(&self, token: String) {
@@ -59,28 +74,104 @@ impl GraphApi {
         self.access_token.read().unwrap().clone()
     }
 
-    pub async fn get_item(&self, item_id: &str) -> algedi_provider_trait::ProviderResult<DriveItem> {
+    pub async fn get_item(
+        &self,
+        item_id: &str,
+    ) -> algedi_provider_trait::ProviderResult<DriveItem> {
         let url = format!("{GRAPH_BASE}/me/drive/items/{item_id}");
         self.get_json(&url).await
     }
 
     pub async fn upload_item(
         &self,
-        _local_path: &std::path::Path,
-        _parent_id: &str,
-    ) -> algedi_provider_trait::ProviderResult<DriveItem> {
-        // TODO: PUT /me/drive/items/{parent-id}:/{name}:/content for small
-        // files; createUploadSession for files above ~4MB.
-        Err(algedi_provider_trait::ProviderError::Other("not implemented".into()))
+        local_path: &Path,
+        parent_id: &str,
+    ) -> ProviderResult<DriveItem> {
+        let name = local_path
+            .file_name()
+            .and_then(|n| n.to_str())
+            .ok_or_else(|| {
+                ProviderError::Other("local path has no valid UTF-8 file name".into())
+            })?;
+        let encoded_name = utf8_percent_encode(name, NON_ALPHANUMERIC).to_string();
+        let size = tokio::fs::metadata(local_path)
+            .await
+            .map_err(io_error)?
+            .len();
+        if size <= SIMPLE_UPLOAD_LIMIT {
+            let body = tokio::fs::read(local_path).await.map_err(io_error)?;
+            let response = self
+                .http
+                .put(format!(
+                    "{GRAPH_BASE}/me/drive/items/{parent_id}:/{encoded_name}:/content"
+                ))
+                .bearer_auth(self.access_token())
+                .body(body)
+                .send()
+                .await
+                .map_err(network_error)?;
+            return decode_json(response, name).await;
+        }
+
+        let response = self.http
+            .post(format!("{GRAPH_BASE}/me/drive/items/{parent_id}:/{encoded_name}:/createUploadSession"))
+            .bearer_auth(self.access_token())
+            .json(&serde_json::json!({"item": {"@microsoft.graph.conflictBehavior": "rename", "name": name}}))
+            .send().await.map_err(network_error)?;
+        let session: UploadSession = decode_json(response, name).await?;
+        let mut file = tokio::fs::File::open(local_path).await.map_err(io_error)?;
+        let mut offset = 0u64;
+        loop {
+            let remaining = (size - offset) as usize;
+            let count = remaining.min(UPLOAD_CHUNK_SIZE);
+            let mut bytes = vec![0u8; count];
+            tokio::io::AsyncReadExt::read_exact(&mut file, &mut bytes)
+                .await
+                .map_err(io_error)?;
+            let end = offset + count as u64 - 1;
+            let response = self
+                .http
+                .put(&session.upload_url)
+                .header(reqwest::header::CONTENT_LENGTH, count)
+                .header(
+                    reqwest::header::CONTENT_RANGE,
+                    format!("bytes {offset}-{end}/{size}"),
+                )
+                .body(bytes)
+                .send()
+                .await
+                .map_err(network_error)?;
+            if response.status() == reqwest::StatusCode::ACCEPTED {
+                offset = end + 1;
+                continue;
+            }
+            return decode_json(response, name).await;
+        }
     }
 
-    pub async fn download_item(
-        &self,
-        _item_id: &str,
-        _dest_path: &std::path::Path,
-    ) -> algedi_provider_trait::ProviderResult<()> {
-        // TODO: GET /me/drive/items/{id}/content, streamed to disk.
-        Err(algedi_provider_trait::ProviderError::Other("not implemented".into()))
+    pub async fn download_item(&self, item_id: &str, dest_path: &Path) -> ProviderResult<()> {
+        let mut response = self
+            .http
+            .get(format!("{GRAPH_BASE}/me/drive/items/{item_id}/content"))
+            .bearer_auth(self.access_token())
+            .send()
+            .await
+            .map_err(network_error)?;
+        if !response.status().is_success() {
+            return Err(response_error(response, item_id).await);
+        }
+        let temporary = temporary_path(dest_path);
+        let mut output = tokio::fs::File::create(&temporary)
+            .await
+            .map_err(io_error)?;
+        while let Some(chunk) = response.chunk().await.map_err(network_error)? {
+            output.write_all(&chunk).await.map_err(io_error)?;
+        }
+        output.flush().await.map_err(io_error)?;
+        drop(output);
+        tokio::fs::rename(&temporary, dest_path)
+            .await
+            .map_err(io_error)
     }
 
     pub async fn delete_item(&self, item_id: &str) -> algedi_provider_trait::ProviderResult<()> {
@@ -95,7 +186,9 @@ impl GraphApi {
         if resp.status().is_success() {
             Ok(())
         } else {
-            Err(algedi_provider_trait::ProviderError::Other(resp.status().to_string()))
+            Err(algedi_provider_trait::ProviderError::Other(
+                resp.status().to_string(),
+            ))
         }
     }
 
@@ -117,10 +210,55 @@ impl GraphApi {
                 .await
                 .map_err(|e| algedi_provider_trait::ProviderError::Other(e.to_string())),
             401 => Err(algedi_provider_trait::ProviderError::AuthRequired),
-            404 => Err(algedi_provider_trait::ProviderError::NotFound(url.to_string())),
-            429 => Err(algedi_provider_trait::ProviderError::RateLimited { retry_after_secs: None }),
-            other => Err(algedi_provider_trait::ProviderError::Other(format!("HTTP {other}"))),
+            404 => Err(algedi_provider_trait::ProviderError::NotFound(
+                url.to_string(),
+            )),
+            429 => Err(algedi_provider_trait::ProviderError::RateLimited {
+                retry_after_secs: None,
+            }),
+            other => Err(algedi_provider_trait::ProviderError::Other(format!(
+                "HTTP {other}"
+            ))),
         }
+    }
+}
+
+fn temporary_path(dest: &Path) -> PathBuf {
+    let mut name = dest.as_os_str().to_owned();
+    name.push(".algedi-part");
+    PathBuf::from(name)
+}
+fn network_error(error: reqwest::Error) -> ProviderError {
+    ProviderError::Network(error.to_string())
+}
+fn io_error(error: std::io::Error) -> ProviderError {
+    ProviderError::Other(error.to_string())
+}
+async fn decode_json<T: for<'de> Deserialize<'de>>(
+    response: reqwest::Response,
+    resource: &str,
+) -> ProviderResult<T> {
+    if response.status().is_success() {
+        response
+            .json()
+            .await
+            .map_err(|e| ProviderError::Other(e.to_string()))
+    } else {
+        Err(response_error(response, resource).await)
+    }
+}
+async fn response_error(response: reqwest::Response, resource: &str) -> ProviderError {
+    let status = response.status();
+    let retry_after_secs = response
+        .headers()
+        .get(reqwest::header::RETRY_AFTER)
+        .and_then(|v| v.to_str().ok())
+        .and_then(|v| v.parse().ok());
+    match status.as_u16() {
+        401 => ProviderError::AuthRequired,
+        404 => ProviderError::NotFound(resource.to_owned()),
+        429 => ProviderError::RateLimited { retry_after_secs },
+        code => ProviderError::Other(format!("HTTP {code}")),
     }
 }
 
@@ -134,5 +272,18 @@ mod tests {
         assert_eq!(api.access_token(), "old-token");
         api.set_access_token("new-token".into());
         assert_eq!(api.access_token(), "new-token");
+    }
+
+    #[test]
+    fn upload_chunks_follow_graph_alignment_requirement() {
+        assert_eq!(UPLOAD_CHUNK_SIZE % (320 * 1024), 0);
+    }
+
+    #[test]
+    fn partial_download_uses_a_sibling_temporary_file() {
+        assert_eq!(
+            temporary_path(Path::new("/tmp/report.pdf")),
+            PathBuf::from("/tmp/report.pdf.algedi-part")
+        );
     }
 }
